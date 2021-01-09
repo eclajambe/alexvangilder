f2534867e9952b914f9a5d6191407ab7a57be442
Evan LaJambe <eclajambe@gmail.com>
2021-01-08 21:32:12 -0800

    Fixed bugged on homepage vector collage that prevented updated images from displaying

--- alex-van-gilder/front-page.php
+++ alex-van-gilder/front-page.php
@@ -28,7 +28,7 @@ get_header(); ?>
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used iaqnstead.
 				 */
-				get_template_part( 'template-parts/content-frontpage', get_post_format() );
+				get_template_part( 'template-parts/content-front-page', get_post_format() );
 				
 			endwhile;
 
@@ -36,7 +36,7 @@ get_header(); ?>
 
 		else :
 	
-			get_template_part( 'template-parts/content-frontpage', 'none' );
+			get_template_part( 'template-parts/content-front-page', 'none' );
 
 		endif; ?>
 
--- alex-van-gilder/template-parts/content-front-page.php
+++ alex-van-gilder/template-parts/content-front-page.php
@@ -12,35 +12,35 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
 	<?php
-    $enable_vc = get_post_meta(get_the_ID(), '_wpb_vc_js_status', true);
-	if(!$enable_vc) {
+    /*$enable_vc = get_post_meta(get_the_ID(), '_wpb_vc_js_status', true);
+	if ( !$enable_vc ) {
 		get_template_part( 'template-parts/content-page-title', 'none' );
-	}
+	}*/
 
-	if( have_rows('image_breakpoints') ): ?>
+	if( have_rows('vector_collage_breakpoints') ): ?>	
 		<div id="vector-collage" class="container mt-5">
 			<div class="row">
 				<div class="col-12">
 			
 					<?php
-						while( have_rows('image_breakpoints') ): the_row(); 
+						while( have_rows('vector_collage_breakpoints') ): the_row(); 
 							
 							$small_image  = get_sub_field( "small_collage_image" );
 							$medium_image = get_sub_field( "medium_collage_image" );
 							$large_image  = get_sub_field( "large_collage_image" );
 
-							if( !empty( $small_image) ) : ?>
-								<img class="d-md-none" src="<?php echo esc_url($small_image['url']); ?>" alt="<?php echo esc_attr($small_image['alt']); ?>" />
+							if( !empty( $small_image) ) : echo $small_image['url']; ?>
+								<br/><img class="d-md-none" src="<?php echo esc_url($small_image['url']); ?>" alt="<?php echo esc_attr($small_image['alt']); ?>" />
 							<?php endif;					
-							if( !empty( $medium_image) ) : ?>
-								<img class="d-none d-md-block d-xl-none" src="<?php echo esc_url($medium_image['url']); ?>" alt="<?php echo esc_attr($medium_image['alt']); ?>" />
+							if( !empty( $medium_image) ) : echo $medium_image['url']; ?>
+								<br/><img class="d-none d-md-block d-xl-none" src="<?php echo esc_url($medium_image['url']); ?>" alt="<?php echo esc_attr($medium_image['alt']); ?>" />
 							<?php endif;
-							if( !empty( $large_image) ) : ?>
-								<img class="d-none d-xl-block" src="<?php echo esc_url($large_image['url']); ?>" alt="<?php echo esc_attr($large_image['alt']); ?>" />
-							
-							<?php
-						endif; 
-					endwhile; ?>
+							if( !empty( $large_image) ) : echo $large_image['url']; ?>
+								<br/><img class="d-none d-xl-block" src="<?php echo esc_url($large_image['url']); ?>" alt="<?php echo esc_attr($large_image['alt']); ?>" />
+							<?php endif; 
+					
+						endwhile; 
+					?>
 
 				</div>					
 			</div>
